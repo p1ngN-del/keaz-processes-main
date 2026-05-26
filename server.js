ddc89a9cd2c8d2d050660afcb19d6846d3174734
p1ngN-del <p1ngN@mail.ru>
2026-05-26 19:14:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 
 // РАЗДАЧА СТАТИКИ (HTML, CSS, JS, JSON)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Health check
 app.get('/health', (req, res) => {
